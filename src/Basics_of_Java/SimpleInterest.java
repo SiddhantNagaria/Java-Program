3c2c7903aab04a1e10c6cebe49936deb7a4e41a1
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-10 17:22:52 +0530

    Simple Interest and Scanner class

---
+++ src/Basics_of_Java/SimpleInterest.java
@@ -0,0 +1,10 @@
+package Basics_of_Java;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        int p = 3000, r = 10 , t= 2;
+        int Si = (p*r*t)/100;
+
+        System.out.println(Si);
+    }
+}
