055b73eb0c0069a9d6640290c1d9c6afda69afd1
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-10 17:37:24 +0530

    char

---
+++ src/Basics_of_Java/Char.java
@@ -0,0 +1,10 @@
+package Basics_of_Java;
+
+public class Char {
+    public static void main(String[] args) {
+        char ch = 'a';
+        System.out.println(ch);
+        int x = ch;
+        System.out.println(x);
+    }
+}
