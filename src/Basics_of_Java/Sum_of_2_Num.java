b926a2d00635b225134d05d380711453500a28f1
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-10 17:09:30 +0530

    sum of 2 num

---
+++ src/Basics_of_Java/Sum_of_2_Num.java
@@ -0,0 +1,10 @@
+package Basics_of_Java;
+
+public class Sum_of_2_Num {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}
