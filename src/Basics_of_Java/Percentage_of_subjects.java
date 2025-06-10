84086e0b99faf8c70f3c3ca56fd395db453dfce3
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-10 17:15:27 +0530

    percentage of subjects

---
+++ src/Basics_of_Java/Percentage_of_subjects.java
@@ -0,0 +1,12 @@
+package Basics_of_Java;
+
+public class Percentage_of_subjects {
+    public static void main(String[] args) {
+        double x1= 89;
+        double x2= 92;
+
+        double p = (x1+x2)/2;
+
+        System.out.println(p);
+    }
+}
