494ffdabfcaf40e491b800ef366b408844029644
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-06-10 17:18:32 +0530

    area of circle

---
+++ src/Basics_of_Java/Area_of_circle.java
@@ -0,0 +1,10 @@
+package Basics_of_Java;
+
+public class Area_of_circle {
+    public static void main(String[] args) {
+        double r = 10;
+        double pi = 3.14;
+        double area  = pi*r*r;
+        System.out.println(area);
+    }
+}
