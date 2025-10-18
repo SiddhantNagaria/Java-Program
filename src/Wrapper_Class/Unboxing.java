76acf13943a3821efafd4459990692a1ac9eb834
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-10-19 01:25:51 +0530

    unboxing in java0

---
+++ src/Wrapper_Class/Unboxing.java
@@ -0,0 +1,12 @@
+package Wrapper_Class;
+
+public class Unboxing {
+    public static void main(String[] args) {
+        Integer a = new Integer(2);
+        Integer code = 109;
+        int aa = a;
+        int ab = a.intValue(); //
+        int empCode = code;
+
+    }
+}
