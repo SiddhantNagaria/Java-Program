4b9bf6eced2d3ec7f17aee5de97f653584fa6af9
SiddhantNagaria <siddhant.nagaria1234@gmail.com>
2025-10-27 22:25:56 +0530

    multithreading methods

---
+++ src/Multi_Threading/Methods/SleepMethod.java
@@ -0,0 +1,24 @@
+package Multi_Threading.Methods;
+
+class Sleep extends Thread {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
+            System.out.println(i);
+        }
+    }
+}
+
+public class SleepMethod {
+    public static void main(String[] args) {
+        Sleep t1 = new Sleep();
+        Sleep t2 = new Sleep();
+        t1.start();
+        t2.start();
+    }
+}
